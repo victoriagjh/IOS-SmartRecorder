96ee69db11f14c68f22c227e01267ea14aed5fcc
victoriagjh <akrso06197@khu.ac.kr>
2019-05-14 23:11:11 +0900

    Add RecordFileListTable
    Add recordFileListView,
    Fixed the recordFileName,
    Approach file directory directly&Reload Files
    Code refactoring

--- Recorder/ViewController.swift
+++ Recorder/ViewController.swift
@@ -1,14 +1,20 @@
 import UIKit
 import AVFoundation
-class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate{
+class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate,UITableViewDelegate,UITableViewDataSource {
     var audioPlayer: AVAudioPlayer?
     var audioRecorder: AVAudioRecorder?
 
+    var recordFiles:[String] = [""]
+    
     @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var RecordFileView: UITableView!
     
+    var dirPaths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+    var recordSettings : [String: AnyObject] = [AVEncoderAudioQualityKey: AVAudioQuality.min.rawValue as AnyObject,AVEncoderBitRateKey: 16 as AnyObject,AVNumberOfChannelsKey: 2 as AnyObject, AVSampleRateKey : 44100.0 as AnyObject]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -16,16 +22,13 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
         playButton.isEnabled = false
         stopButton.isEnabled = false
         
-        let dirPaths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
-        let docsDir = dirPaths[0] as String
-        let soundFilePath = dirPaths[0] + "/test.wav"
+        _ = dirPaths[0] as String
+        
+        let soundFilePath = dirPaths[0]
         let soundFileURL = NSURL(fileURLWithPath: soundFilePath)
-        print(soundFileURL)
-        print(soundFilePath)
         
-        let recordSettings : [String: AnyObject] = [AVEncoderAudioQualityKey: AVAudioQuality.min.rawValue as AnyObject,AVEncoderBitRateKey: 16 as AnyObject,AVNumberOfChannelsKey: 2 as AnyObject, AVSampleRateKey : 44100.0 as AnyObject]
+        recordFiles = getRecordFileList() as! [String]
 
-        
         let audioSession = AVAudioSession.sharedInstance()
         do {
             try audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
@@ -33,7 +36,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
             print("AudioSession Error")
         }
         do{
-            try audioRecorder = AVAudioRecorder(url: soundFileURL as URL, settings: recordSettings as! [String: AnyObject])
+            try audioRecorder = AVAudioRecorder(url: soundFileURL as URL, settings: recordSettings )
             audioRecorder?.prepareToRecord()
 
         } catch {
@@ -47,15 +50,49 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
 
     @IBAction func recordButton(_ sender: Any) {
         if audioRecorder?.isRecording == false {
+            let soundFileURL = NSURL(fileURLWithPath: makeSoundFileURL())
+            do{
+                try audioRecorder = AVAudioRecorder(url: soundFileURL as URL, settings: recordSettings )
+                audioRecorder?.prepareToRecord()
+            } catch {
+                print("AudioRecorder error")
+            }
             recordButton.isEnabled = false
             playButton.isEnabled = false
             stopButton.isEnabled = true
             audioRecorder?.record()
-            
         }
+    }
+    func makeSoundFileURL() -> String {
+        let date:Date = Date()
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "yyyy_MM_dd hh:mm:ss"
+        formatter.timeZone = NSTimeZone(name:"UTC") as TimeZone?
+        let dateString = formatter.string(from: date)
         
+        let soundFilePath = dirPaths[0] + "/"+dateString+".wav"
+        return soundFilePath
     }
     
+    func getRecordFileList() -> Array<Any> {
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        do{
+            let directoryContents = try FileManager.default.contentsOfDirectory(at: documentsURL, includingPropertiesForKeys: nil, options:[])
+            print(directoryContents)
+            
+            let wavFiles = directoryContents.filter{$0.pathExtension == "wav"}
+            print("wav File", wavFiles,"\n")
+            let wavFileNames = wavFiles.map{ $0.deletingPathExtension().lastPathComponent }
+            print("wavFile list", wavFileNames,"\n")
+            return wavFileNames
+            
+        }catch{
+            print(error)
+        }
+        return ["ERROR"]
+    }
+
     @IBAction func stopButton(_ sender: Any) {
         stopButton.isEnabled = false
         playButton.isEnabled = true
@@ -63,6 +100,8 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
         
         if audioRecorder?.isRecording == true {
             audioRecorder?.stop()
+            recordFiles = getRecordFileList() as! [String]
+            RecordFileView.reloadData()
         } else {
             print("Audio Recorder was not working")
         }
@@ -95,5 +134,23 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
     func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
         print("Audio")
     }
+    
+    //SETTING UP TABLE VIEW
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recordFiles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "recordCell",for: indexPath)
+        let fileNames = recordFiles
+        let fileName = fileNames[indexPath.row]
+        cell.textLabel!.text = fileName //!는 optional이기 때문에 넣어주는 값임
+        if let capacity:String = fileName{
+            cell.detailTextLabel?.text = "\(capacity)"
+        }
+        return cell
+    }
+    
 }
 
